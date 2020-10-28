6846f2a492a02d9590d1cdfed7973370c929711c
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-10-28 15:48:36 +0200

    Pack DB object into a response object and return

--- controller/productController.js
+++ controller/productController.js
@@ -1,6 +1,21 @@
 const productService = require('../service/productService');
 
-module.exports.createProduct = (req, res) => {
-    console.log('=== ', req.body);
-    productService.createProduct(req.body);
+module.exports.createProduct = async (req, res) => {
+    let response = {};
+    try {
+        console.log('Request OBJ: ', req.body);
+        const serviceResponse = await productService.createProduct(req.body);
+
+        response.status = 200;
+        response.message = 'Product created successfully';
+        response.body = serviceResponse;
+    } catch (error) {
+        console.log('Something went wrong: Controller: createProduct', error);
+
+        response.status = 400;
+        response.message = error.message;
+        response.body = {};
+    }
+    // return response
+    return res.status(response.status).send(response);
 }
