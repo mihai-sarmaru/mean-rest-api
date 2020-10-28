249c5ccc5186705250a0720f6b279512565c5fc9
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-10-28 15:35:42 +0200

    Insetr new product to DB and return object

--- service/productService.js
+++ service/productService.js
@@ -1,3 +1,12 @@
-module.exports.createProduct = (product) => {
-    
+const Product = require('../model/product');
+
+module.exports.createProduct = async (productData) => {
+    try {
+        // destructure
+        let product = new Product({ ...productData });
+        return await product.save();
+    } catch (error) {
+        console.log('Something went wrong: Service: createProduct', error);
+        throw new Error(error);
+    }
 }
