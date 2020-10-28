24c61947fe4b5349bf9f084afa1b61d92ce77b16
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-10-28 16:01:04 +0200

    Extract response and message to constants

--- controller/productController.js
+++ controller/productController.js
@@ -1,20 +1,18 @@
 const productService = require('../service/productService');
+const constants = require('../constants/constants');
 
 module.exports.createProduct = async (req, res) => {
-    let response = {};
+    let response = { ...constants.defaultServerResponse };
     try {
         console.log('Request OBJ: ', req.body);
         const serviceResponse = await productService.createProduct(req.body);
 
         response.status = 200;
-        response.message = 'Product created successfully';
+        response.message = constants.productMessage.PRODUCT_CREATED;
         response.body = serviceResponse;
     } catch (error) {
         console.log('Something went wrong: Controller: createProduct', error);
-
-        response.status = 400;
         response.message = error.message;
-        response.body = {};
     }
     // return response
     return res.status(response.status).send(response);
