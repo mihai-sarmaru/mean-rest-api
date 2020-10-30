9051c74d9645fef1d97a96c96cff197218d6ae84
Mihai Sarmaru <mihai.sarmaru@gmail.com>
2020-10-30 17:01:02 +0200

    Validate routes using JsonWebToken

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -3,21 +3,29 @@ const router = express.Router();
 const productController = require('../controller/productController');
 const joiSchemaValidation = require('../middleware/joiSchemaValidation');
 const productSchema = require('../schema/productSchema');
+const tokenValidation = require('../middleware/tokenValidation');
 
 router.post('/',
+    tokenValidation.validateToken,
     joiSchemaValidation.validateBody(productSchema.createProductSchema),
     productController.createProduct);
 
 router.get('/',
+    tokenValidation.validateToken,
     joiSchemaValidation.validateQueryParams(productSchema.getAllProductsSchema),
     productController.getAllProducts);
 
-router.get('/:id', productController.getProductById);
+router.get('/:id',
+    tokenValidation.validateToken,
+    productController.getProductById);
 
 router.put('/:id',
+    tokenValidation.validateToken,
     joiSchemaValidation.validateBody(productSchema.updateProductSchema),
     productController.updateProduct);
 
-router.delete('/:id', productController.deleteProduct);
+router.delete('/:id',
+    tokenValidation.validateToken,
+    productController.deleteProduct);
 
 module.exports = router;
